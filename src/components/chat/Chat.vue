493b0f045a2f23bd9597a42b6baafed82a1004bd
cswccc <3344767250@qq.com>
2023-12-31 22:40:00 +0800

    delete chat

--- src/components/chat/Chat.vue
+++ src/components/chat/Chat.vue
@@ -1,164 +1,164 @@
-<template>
-  <div>
-    <beautiful-chat
-        :participants="participants"
-        :titleImageUrl="titleImageUrl"
-        :onMessageWasSent="sendMessage"
-        :messageList="messageList"
-        :newMessagesCount="newMessagesCount"
-        :isOpen="isChatOpen"
-        :close="closeChat"
-        :open="openChat"
-        :showEdition="true"
-        :showDeletion="true"
-        :showTypingIndicator="showTypingIndicator"
-        :showLauncher="true"
-        :showCloseButton="false"
-        :colors="colors"
-        :alwaysScrollToBottom="alwaysScrollToBottom"
-        :disableUserListToggle="false"
-        :messageStyling="messageStyling"
-        @onType="handleOnType"
-        @edit="editMessage"/>
-  </div>
-</template>
+<!--<template>-->
+<!--  <div>-->
+<!--    <beautiful-chat-->
+<!--        :participants="participants"-->
+<!--        :titleImageUrl="titleImageUrl"-->
+<!--        :onMessageWasSent="sendMessage"-->
+<!--        :messageList="messageList"-->
+<!--        :newMessagesCount="newMessagesCount"-->
+<!--        :isOpen="isChatOpen"-->
+<!--        :close="closeChat"-->
+<!--        :open="openChat"-->
+<!--        :showEdition="true"-->
+<!--        :showDeletion="true"-->
+<!--        :showTypingIndicator="showTypingIndicator"-->
+<!--        :showLauncher="true"-->
+<!--        :showCloseButton="false"-->
+<!--        :colors="colors"-->
+<!--        :alwaysScrollToBottom="alwaysScrollToBottom"-->
+<!--        :disableUserListToggle="false"-->
+<!--        :messageStyling="messageStyling"-->
+<!--        @onType="handleOnType"-->
+<!--        @edit="editMessage"/>-->
+<!--  </div>-->
+<!--</template>-->
 
-<script>
-import axiosInstance from "@/main";
-const AdminUserMail = '3077161150@qq.com';
+<!--<script>-->
+<!--import axiosInstance from "@/main";-->
+<!--const AdminUserMail = '3077161150@qq.com';-->
 
-export default {
-  data() {
-    return {
-      userMail: null,
-      // Other data properties...
-      socket: null,
-      participants: [
-        {
-          id: 'user1',
-          name: 'Matteo',
-          imageUrl: 'https://avatars3.githubusercontent.com/u/1915989?s=230&v=4'
-        },
-        {
-          id: 'user2',
-          name: 'Support',
-          imageUrl: 'https://avatars3.githubusercontent.com/u/37018832?s=200&v=4'
-        }
-      ], // 对话的所有参与者的列表。' name '是用户名，' id '用于建立消息的作者，' imageUrl '应该是用户头像。
-      titleImageUrl: 'https://a.slack-edge.com/66f9/img/avatars-teams/ava_0001-34.png',
-      messageList: [
-        {type: 'text', author: `me`, data: {text: `Say yes!`}, toUserMail: '3344767250@qq.com'},
-        {type: 'text', author: `user1`, data: {text: `No.`}, toUserMail: '3344767250@qq.com'}
-      ], // // 要显示的消息列表可以动态地分页和调整
-      newMessagesCount: 0,
-      isChatOpen: false, // 确定聊天窗口应该打开还是关闭
-      showTypingIndicator: '',  // 当设置为匹配参与者的值时。它显示特定用户的输入指示
-      colors: {
-        header: {
-          bg: '#4e8cff',
-          text: '#ffffff'
-        },
-        launcher: {
-          bg: '#4e8cff'
-        },
-        messageList: {
-          bg: '#ffffff'
-        },
-        sentMessage: {
-          bg: '#4e8cff',
-          text: '#ffffff'
-        },
-        receivedMessage: {
-          bg: '#eaeaea',
-          text: '#222222'
-        },
-        userInput: {
-          bg: '#f4f7f9',
-          text: '#565867'
-        }
-      }, // specifies the color scheme for the component
-      alwaysScrollToBottom: false, // 当设置为true时，当有新事件发生时(新消息，用户开始输入…)，总是将聊天滚动到底部。
-      messageStyling: true // 启用*bold* /emph/ _underline_等(更多信息请访问github.com/mattezza/msgdown)
-    }
-  },
-  methods: {
-    handleWebSocketMessage(event) {
-      const message = JSON.parse(event.data);
-      this.onMessageWasSent(message);
-    },
-    sendMessage(message) {
-      // console.log(message.data.text.length)
-      if (message.data.text.length > 0) {
-        this.newMessagesCount = this.isChatOpen ? this.newMessagesCount : this.newMessagesCount + 1
+<!--export default {-->
+<!--  data() {-->
+<!--    return {-->
+<!--      userMail: null,-->
+<!--      // Other data properties...-->
+<!--      socket: null,-->
+<!--      participants: [-->
+<!--        {-->
+<!--          id: 'user1',-->
+<!--          name: 'Matteo',-->
+<!--          imageUrl: 'https://avatars3.githubusercontent.com/u/1915989?s=230&v=4'-->
+<!--        },-->
+<!--        {-->
+<!--          id: 'user2',-->
+<!--          name: 'Support',-->
+<!--          imageUrl: 'https://avatars3.githubusercontent.com/u/37018832?s=200&v=4'-->
+<!--        }-->
+<!--      ], // 对话的所有参与者的列表。' name '是用户名，' id '用于建立消息的作者，' imageUrl '应该是用户头像。-->
+<!--      titleImageUrl: 'https://a.slack-edge.com/66f9/img/avatars-teams/ava_0001-34.png',-->
+<!--      messageList: [-->
+<!--        {type: 'text', author: `me`, data: {text: `Say yes!`}, toUserMail: '3344767250@qq.com'},-->
+<!--        {type: 'text', author: `user1`, data: {text: `No.`}, toUserMail: '3344767250@qq.com'}-->
+<!--      ], // // 要显示的消息列表可以动态地分页和调整-->
+<!--      newMessagesCount: 0,-->
+<!--      isChatOpen: false, // 确定聊天窗口应该打开还是关闭-->
+<!--      showTypingIndicator: '',  // 当设置为匹配参与者的值时。它显示特定用户的输入指示-->
+<!--      colors: {-->
+<!--        header: {-->
+<!--          bg: '#4e8cff',-->
+<!--          text: '#ffffff'-->
+<!--        },-->
+<!--        launcher: {-->
+<!--          bg: '#4e8cff'-->
+<!--        },-->
+<!--        messageList: {-->
+<!--          bg: '#ffffff'-->
+<!--        },-->
+<!--        sentMessage: {-->
+<!--          bg: '#4e8cff',-->
+<!--          text: '#ffffff'-->
+<!--        },-->
+<!--        receivedMessage: {-->
+<!--          bg: '#eaeaea',-->
+<!--          text: '#222222'-->
+<!--        },-->
+<!--        userInput: {-->
+<!--          bg: '#f4f7f9',-->
+<!--          text: '#565867'-->
+<!--        }-->
+<!--      }, // specifies the color scheme for the component-->
+<!--      alwaysScrollToBottom: false, // 当设置为true时，当有新事件发生时(新消息，用户开始输入…)，总是将聊天滚动到底部。-->
+<!--      messageStyling: true // 启用*bold* /emph/ _underline_等(更多信息请访问github.com/mattezza/msgdown)-->
+<!--    }-->
+<!--  },-->
+<!--  methods: {-->
+<!--    handleWebSocketMessage(event) {-->
+<!--      const message = JSON.parse(event.data);-->
+<!--      this.onMessageWasSent(message);-->
+<!--    },-->
+<!--    sendMessage(message) {-->
+<!--      // console.log(message.data.text.length)-->
+<!--      if (message.data.text.length > 0) {-->
+<!--        this.newMessagesCount = this.isChatOpen ? this.newMessagesCount : this.newMessagesCount + 1-->
 
-        const packMessage = {
-          toUserMail: AdminUserMail,
-          content: message.data.text,
-        }
-        // 发送消息到后端
-        this.socket.send(JSON.stringify(packMessage))
+<!--        const packMessage = {-->
+<!--          toUserMail: AdminUserMail,-->
+<!--          content: message.data.text,-->
+<!--        }-->
+<!--        // 发送消息到后端-->
+<!--        this.socket.send(JSON.stringify(packMessage))-->
 
-        this.onMessageWasSent(message);
-      }
-    },
-    onMessageWasSent(message) {
-      console.log(message)
-      // 当用户发送消息时调用
-      // console.log(message)
-      this.messageList = [...this.messageList, message]
-    },
-    openChat() {
-      // 当用户单击fab按钮打开聊天时调用
-      this.isChatOpen = true
-      this.newMessagesCount = 0
+<!--        this.onMessageWasSent(message);-->
+<!--      }-->
+<!--    },-->
+<!--    onMessageWasSent(message) {-->
+<!--      console.log(message)-->
+<!--      // 当用户发送消息时调用-->
+<!--      // console.log(message)-->
+<!--      this.messageList = [...this.messageList, message]-->
+<!--    },-->
+<!--    openChat() {-->
+<!--      // 当用户单击fab按钮打开聊天时调用-->
+<!--      this.isChatOpen = true-->
+<!--      this.newMessagesCount = 0-->
 
-      this.initSocket()
-    },
-    closeChat() {
-      // // 当用户单击按钮关闭聊天时调用
-      this.isChatOpen = false
+<!--      this.initSocket()-->
+<!--    },-->
+<!--    closeChat() {-->
+<!--      // // 当用户单击按钮关闭聊天时调用-->
+<!--      this.isChatOpen = false-->
 
-      if (this.socket) {
-        this.socket.close();
-        this.socket = null;
-      }
-    },
-    handleScrollToTop() {
+<!--      if (this.socket) {-->
+<!--        this.socket.close();-->
+<!--        this.socket = null;-->
+<!--      }-->
+<!--    },-->
+<!--    handleScrollToTop() {-->
 
-      // 当用户将消息列表滚动到顶部时调用
-// 利用分页来加载另一个消息页面
+<!--      // 当用户将消息列表滚动到顶部时调用-->
+<!--// 利用分页来加载另一个消息页面-->
 
-    },
-    handleOnType() {
-      console.log('Emit typing event')
-    },
-    editMessage(message) {
-      const m = this.messageList.find(m => m.id === message.id);
-      m.isEdited = true;
-      m.data.text = message.data.text;
-    },
-    getUserMail() {
-      console.log(666)
-      console.log(localStorage.getItem('passToken'))
-      return axiosInstance.get(this.$httpUrl + 'TheLoginUserInfo/getLoginUserMail')
-          .then(res => res.data)
-          .then(res => {
-            if (res.code === 200) {
-              console.log(res.data)
-              this.userMail = res.data;
-              console.log(this.userMail);
-              return res.data; // 返回用户邮件
-            }
-          });
-    },
-    initSocket() {
-      this.getUserMail().then(() => {
-        // 在获取用户邮件后创建 WebSocket 连接
-        console.log(this.userMail)
-        this.socket = new WebSocket(`ws://localhost:8081/ws/${this.userMail}?passToken=${localStorage.getItem('passToken')}`);
-        this.socket.addEventListener('message', this.handleWebSocketMessage);
-      });
-    },
-  },
-};
-</script>
+<!--    },-->
+<!--    handleOnType() {-->
+<!--      console.log('Emit typing event')-->
+<!--    },-->
+<!--    editMessage(message) {-->
+<!--      const m = this.messageList.find(m => m.id === message.id);-->
+<!--      m.isEdited = true;-->
+<!--      m.data.text = message.data.text;-->
+<!--    },-->
+<!--    getUserMail() {-->
+<!--      console.log(666)-->
+<!--      console.log(localStorage.getItem('passToken'))-->
+<!--      return axiosInstance.get(this.$httpUrl + 'TheLoginUserInfo/getLoginUserMail')-->
+<!--          .then(res => res.data)-->
+<!--          .then(res => {-->
+<!--            if (res.code === 200) {-->
+<!--              console.log(res.data)-->
+<!--              this.userMail = res.data;-->
+<!--              console.log(this.userMail);-->
+<!--              return res.data; // 返回用户邮件-->
+<!--            }-->
+<!--          });-->
+<!--    },-->
+<!--    initSocket() {-->
+<!--      this.getUserMail().then(() => {-->
+<!--        // 在获取用户邮件后创建 WebSocket 连接-->
+<!--        console.log(this.userMail)-->
+<!--        this.socket = new WebSocket(`ws://localhost:8081/ws/${this.userMail}?passToken=${localStorage.getItem('passToken')}`);-->
+<!--        this.socket.addEventListener('message', this.handleWebSocketMessage);-->
+<!--      });-->
+<!--    },-->
+<!--  },-->
+<!--};-->
+<!--</script>-->
