b73432c7447207f02cc935c8fdaf94b4082f4895
yfyHaoYe <2926687667@qq.com>
2024-01-01 14:25:53 +0800

    video uml changed

---
+++ src/views/Comment.vue
@@ -0,0 +1,12 @@
+<script setup>
+
+import CommentArea from "@/components/comments/commentArea.vue";
+</script>
+
+<template>
+  <comment-area :department="0"/>
+</template>
+
+<style scoped>
+
+</style>
